a487c1c6f3f42df6dd538805ab2398e159e06069
Carsten Witt <carsten.witt@gmail.com>
2017-01-27 12:19:44 +0100

    PSR-3 Logging

--- src/SwiftMailerCallable.php
+++ src/SwiftMailerCallable.php
@@ -1,7 +1,11 @@
 <?php
 namespace Germania\SwiftMailerCallable;
 
-class SwiftMailerCallable
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Psr\Log\LoggerAwareInterface;
+
+class SwiftMailerCallable implements LoggerAwareInterface
 {
 
     /**
@@ -14,16 +18,22 @@ class SwiftMailerCallable
      */
     public $message_factory;
 
+    /**
+     * @var LoggerInterface
+     */
+    public $logger;
+
 
 
     /**
      * @param Swift_Mailer $mailer
      * @param Callable $message_factory Factory returning new Swift_Message instance
      */
-    public function __construct( \Swift_Mailer $mailer, Callable $message_factory )
+    public function __construct( \Swift_Mailer $mailer, Callable $message_factory, LoggerInterface $logger = null )
     {
         $this->mailer  = $mailer;
         $this->message_factory = $message_factory;
+        $this->setLogger($logger ?: new NullLogger);
     }
 
     /**
@@ -59,6 +69,25 @@ class SwiftMailerCallable
             $message->setTo( $to );
         }
 
-        return $this->mailer->send( $message );
+
+        if ($result = $this->mailer->send( $message )):
+            $this->logger->info("Successfully sent mail", ['to' => implode(", ", $to) ]);
+        else:
+            $this->logger->error("Failed sending mail", ['to' => implode(", ", $to) ]);
+        endif;
+
+        return $result;
+    }
+
+    /**
+     * Sets a logger instance on the object.
+     *
+     * @param LoggerInterface $logger
+     *
+     * @return void
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
     }
 }
